5a62ecf4b337f15085d39251a22af3f1d0de40f6
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-11-23 15:27:49 +0100

    Delete unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import './styles.css';
 
 // Just to have a template for the file importing
-import { greeting } from './greeting.js';
-console.log(greeting);
+// import { greeting } from './greeting.js';
+// console.log(greeting);
 
 // Another webpack check
 if (process.env.NODE_ENV !== 'production') {
